17c00f7eb69fb487a856a5fe459ace9e5cd1c05b
John Doe <johndoe@example.com>
2023-04-22 12:45:34 +0200

    password encryption implementation

--- module-2/week-4/express-review/routes.user.js
+++ module-2/week-4/express-review/routes.user.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const User = require('./models/User');
 
@@ -7,13 +9,52 @@ router.get('/user/register', (request, response) => {
   response.render('user/page-register');
 });
 
-router.post('/user/register', (request, response) => {
-  const { email, password } = request.body;
-  User.create({
-    email: email,
-    // password: password
-  })
-  console.log(email, password);
+
+  router.post("/user/register", (req, res) => {
+    const { email, password } = req.body;  
+
+  // Check that username, email, and password are provided
+  if (email === "" || password === "") {
+    res.status(400).render("user/page-register", {
+      errorMessage:
+        "All fields are mandatory. Please provide your username, email and password.",
+    });
+
+    return;
+  }
+
+  if (password.length < 6) {
+    res.status(400).render("user/page-register", {
+      errorMessage: "Your password needs to be at least 6 characters long.",
+    });
+
+    return;
+  }
+
+  // Create a new user - start by hashing the password
+  bcrypt
+    .genSalt(saltRounds)
+    .then((salt) => bcrypt.hash(password, salt))
+    .then((hashedPassword) => {
+      // Create a user and save it in the database
+      return User.create({ email, password: hashedPassword });
+    })
+    .then((user) => {
+      res.redirect("/profile");
+    })
+    .catch((error) => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(500).render("user/page-register", { errorMessage: error.message });
+      } else if (error.code === 11000) {
+        res.status(500).render("user/page-register", {
+          errorMessage:
+            "Username and email need to be unique. Provide a valid username or email.",
+        });
+      } else {
+        next(error);
+      }
+    });
 });
+
 
 module.exports = router;
